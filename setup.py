150afc8272a3a9d9636efdb55e2b4ba5a7fd940d
idle sign <idlesign@yandex.ru>
2013-02-27 20:53:06 +0700

    README is not rst marked (closes #1).

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 from sitemetrics import VERSION
 
-f = open(os.path.join(os.path.dirname(__file__), 'README'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
 f.close()
 
